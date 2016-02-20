404d508a4df7efc682fc65f52c0afad3bc93d31b
Juan José López Jaimez <Juan Jos Lpez Jaimez>
2016-02-20 15:39:50 -0600

    aa

---
+++ Marmota/src/Servidor.java
@@ -0,0 +1,17 @@
+
+import java.net.*;
+/**
+ * Clase principal del servidor
+ * creada a las 3:15 pm 
+ * Funcionalidades:
+ *  Login
+ * @author JuanJosé
+ */
+public class Servidor{
+    final int puerto = 7890;
+    public static void main(String[] args){
+                
+    }
+
+   
+}
